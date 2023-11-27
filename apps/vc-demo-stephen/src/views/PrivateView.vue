01e393e6446a12766d0ae5f50f75d8d83d9fe8a6
Stephen Adkins <spadkins@gmail.com>
2023-11-27 12:34:22 -0500

    Login Page is working

---
+++ apps/vc-demo-stephen/src/views/PrivateView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="private">
+    <h1>This is a private page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .private {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
